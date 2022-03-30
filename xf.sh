a391a424ff48f66f58b9c627db42ad4615766b5a
usachoc <102590097+usachoc@users.noreply.github.com>
2022-03-30 09:42:43 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "a7ae116b-9e81-4491-9273-d4e9eac3ce57",
                         "alterId": 0
                     }
                 ],
